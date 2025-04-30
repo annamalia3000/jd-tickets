16011fd525d6f2e8597a4e9cccd098c8fa0a28c8
annamalia3000 <a.molch@mail.ru>
2025-04-30 16:11:47 +0300

    feat: add city input

---
+++ src/components/CityInput/CityInput.tsx
@@ -0,0 +1,111 @@
+import { useState, useEffect } from "react";
+import LocationIcon from "../../assets/icons/location.svg?react";
+import classes from "./cityInput.module.css";
+
+type CityInputProps = {
+  value: string;
+  onChange: (value: string) => void;
+  startPlaceholder?: string;
+  name?: string;
+};
+
+type CityProps = {
+  _id: "string";
+  name: "string";
+};
+
+export const CityInput = ({
+  value,
+  onChange,
+  startPlaceholder = "",
+  name = "",
+}: CityInputProps) => {
+  const [suggestions, setSuggestions] = useState<CityProps[]>([]);
+  const [showSuggestions, setShowSuggestions] = useState(true);
+  const url = import.meta.env.VITE_HOST;
+  const apiURL = `${url}/routes/cities?name=${value} `;
+
+  useEffect(() => {
+    const fetchCities = async () => {
+      if (!value.trim()) {
+        setSuggestions([]);
+        return;
+      }
+
+      try {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        setSuggestions(data);
+      } catch (error) {
+        console.log("Ошибка загрузки городов", error);
+      }
+    };
+    const timeout = setTimeout(fetchCities, 1 * 1000);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [value]);
+
+  const handleSelect = (cityName: string) => {
+    onChange(cityName);
+    setSuggestions([]);
+    setShowSuggestions(false);
+  };
+
+  const matchedSuggestion = suggestions.find((city: CityProps) =>
+    city.name.toLowerCase().startsWith(value.toLowerCase())
+  );
+
+
+  return (
+    <div className={classes["city__input-container"]}>
+     <div className={classes["city__ghost-text"]}>
+  {matchedSuggestion && matchedSuggestion.name.toLowerCase().startsWith(value.toLowerCase()) ? (
+    <>
+      <span className={classes["city__ghost-transparent"]}>
+        {matchedSuggestion.name.slice(0, value.length)}
+      </span>
+      <span className={classes["city__ghost-visible"]}>
+      {matchedSuggestion.name.slice(value.length)}
+      </span>
+
+    </>
+  ) : null}
+</div>
+
+
+      <input
+        type="text"
+        name={name}
+        placeholder={startPlaceholder}
+        value={value}
+        autoComplete="off"
+        className={classes["city__input"]}
+        onChange={(e) => {
+          onChange(e.target.value);
+          setShowSuggestions(true);
+        }}
+        onFocus={() => setShowSuggestions(true)}
+        // onBlur={() => {
+        //   setTimeout(() => {
+        //     setShowSuggestions(false);
+        //   }, 200);
+        // }}
+      />
+      <LocationIcon className={classes["city__input-icon"]} />
+      {suggestions.length > 0 && showSuggestions && (
+        <ul className={classes["city__list"]}>
+          {suggestions.map((city: CityProps) => (
+            <li
+              key={city._id}
+              onClick={() => handleSelect(city.name)}
+              className={classes["city__item"]}
+            >
+              {city.name}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
--- src/components/Direction/Direction.tsx
+++ src/components/Direction/Direction.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import { CityInput } from "../CityInput/CityInput";
 import SwapIcon from "../../assets/icons/remove.svg?react";
-import LocationIcon from "../../assets/icons/location.svg?react";
+
 import classes from "./direction.module.css";
 
 export const Direction = () => {
@@ -25,34 +26,14 @@ export const Direction = () => {
     <form action={handleSubmit} className={classes["direction"]}>
       <h4 className={classes["direction__title"]}>Направление</h4>
       <div className={classes["direction__inputs"]}>
-        <div className={classes["direction__input-container"]}>
-          <input
-            type="text"
-            name="from"
-            placeholder="Откуда"
-            className={classes["direction__input"]}
-            value={from}
-            onChange={(e) => setFrom(e.target.value)}
-          />
-          <LocationIcon className={classes["direction__input-icon"]} />
-        </div>
+        <CityInput value={from} onChange={setFrom} startPlaceholder="Откуда" name="from" />
         <button
           className={classes["direction__swap-button"]}
           onClick={swapDirection}
         >
           <SwapIcon className={classes["direction__swap-icon"]} />
         </button>
-        <div className={classes["direction__input-container"]}>
-          <input
-            type="text"
-            name="to"
-            placeholder="Куда"
-            value={to}
-            className={classes["direction__input"]}
-            onChange={(e) => setTo(e.target.value)}
-          />
-          <LocationIcon className={classes["direction__input-icon"]} />
-        </div>
+        <CityInput value={to} onChange={setTo} startPlaceholder="Куда" name="to" />
       </div>
     </form>
   );
